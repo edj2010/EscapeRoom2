3bb182627d077f24af90b7d53b854cb55dab117c
Owen Gillespie <owenlgillespie@gmail.com>
2018-06-18 20:42:08 -0400

    use database to store outgoing values. Closes #5

--- models.py
+++ models.py
@@ -2,9 +2,11 @@ from sqlalchemy import *
 
 metadata = MetaData()
 
-nodes_table = Table('nodes_table', metadata,
-    Column('client_id', Integer, primary_key=True),
-    Column('client_name', String),
-    Column('last_ping', DateTime),
-    Column('out_val', String))
-
+nodes_table = Table(
+    "nodes_table",
+    metadata,
+    Column("client_id", Integer, primary_key=True),
+    Column("client_name", String),
+    Column("last_ping", DateTime),
+    Column("out_val", String),
+)
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import json
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, select
 from collections import defaultdict
 import serverlogic
 from flask import Flask
@@ -8,11 +8,16 @@ import datetime
 
 app = Flask(__name__)
 
+
 def heartbeatHandler(nodeName):
     conn = pServer.engine.connect()
-    stmt = nodes_table.update().where(nodes_table.c.client_id == pServer.nodes[nodeName]).values(last_ping = datetime.datetime.now())
+    stmt = (
+        nodes_table.update()
+        .where(nodes_table.c.client_id == pServer.nodes[nodeName])
+        .values(last_ping=datetime.datetime.now())
+    )
     conn.execute(stmt)
-    
+
 
 @app.route("/")
 def welcome():
@@ -25,7 +30,13 @@ def input(nodeName, value):
     outputNodes = pServer.mappings[nodeName]
     for outputNode in outputNodes:
         if outputNode in pServer.nodes:
-            pServer.outputs[outputNode] = value
+            conn = pServer.engine.connect()
+            updtStmt = (
+                nodes_table.update()
+                .where(nodes_table.c.client_id == pServer.nodes[outputNode])
+                .values(out_val=value)
+            )
+            conn.execute(updtStmt)
         else:
             raise KeyError("{0} is not a node".format(outputNode))
             # TODO: Change this to a http error code
@@ -36,7 +47,10 @@ def input(nodeName, value):
 def output(nodeName):
     heartbeatHandler(nodeName)
     if nodeName in pServer.nodes:
-        return str(pServer.outputs[nodeName])
+        conn = pServer.engine.connect()
+        selStmt = select([nodes_table]).where(nodes_table.c.client_name == nodeName)
+        results = conn.execute(selStmt)
+        return results.fetchone()["out_val"]
     else:
         raise KeyError("{0} is not a node".format(nodeName))
         # TODO: Change this to a http error code
@@ -57,19 +71,25 @@ class PuzzleServer:
     def __init__(self, filename):
         with open(filename, "r") as infile:
             connections = json.load(infile)
-        self.nodes = {node:ID for ID, node in enumerate(connections["Nodes"])}
+        self.nodes = {node: ID for ID, node in enumerate(connections["Nodes"])}
         self.streams = connections["Streams"]
         self.mappings = connections["Mappings"]
         self.outputs = defaultdict(str)
         self.localVals = {}
-        self.engine = create_engine('sqlite:///server.db', echo=True)
-        if not self.engine.dialect.has_table(self.engine, 'nodes_table'):
+        self.engine = create_engine("sqlite:///server.db", echo=True)
+        if not self.engine.dialect.has_table(self.engine, "nodes_table"):
             metadata.bind = self.engine
             metadata.create_all(self.engine)
             i = nodes_table.insert()
             for node in self.nodes:
-                i.execute({'client_id': self.nodes[node], 'client_name': node, 'last_ping': datetime.datetime.now(), 'out_val': ""})
-            
-            
+                i.execute(
+                    {
+                        "client_id": self.nodes[node],
+                        "client_name": node,
+                        "last_ping": datetime.datetime.now(),
+                        "out_val": "",
+                    }
+                )
+
 
 pServer = PuzzleServer("connections.json")
